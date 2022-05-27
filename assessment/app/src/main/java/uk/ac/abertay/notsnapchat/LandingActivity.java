306f988cb840d93e8795f4428c8ebff5bbd27cd3
Ben Fleuty <bfleuty@outlook.com>
2022-05-27 06:02:35 +0100

    Add landing page with skeleton logic for login

---
+++ assessment/app/src/main/java/uk/ac/abertay/notsnapchat/LandingActivity.java
@@ -0,0 +1,53 @@
+package uk.ac.abertay.notsnapchat;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        TextView txt = findViewById(R.id.txtWelcomeMessage);
+        String msg = getString(R.string.landing_welcome) + " " + getString(R.string.app_name);
+        txt.setText(msg);
+
+        switch (userLoggedInStatus()) {
+            case LoggedIn:
+                // todo start main app activity
+                break;
+            case NotLoggedIn:
+                // todo start login activity
+                break;
+            case SessionExpired:
+                // todo start login activity with session expired message 
+                break;
+            default:
+                // todo error message
+        }
+
+    }
+
+    private LoginStatus userLoggedInStatus() {
+        // if the user has logged in before, they will have an auth key
+        // todo check for auth key with database
+
+        // if the user's key is valid then they can log in
+        // todo return user log in
+
+        // if the user's key is invalid then the session has expired
+        // todo return expired session
+
+        // otherwise the user has no auth key and therefore has not logged in before
+        return LoginStatus.NotLoggedIn;
+    }
+
+    enum LoginStatus {
+        LoggedIn,
+        NotLoggedIn,
+        SessionExpired
+    }
+}
