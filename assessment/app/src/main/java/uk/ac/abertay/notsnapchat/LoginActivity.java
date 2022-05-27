ac750bf4d06a104f9296be994e7f1f3b03b65cb9
Ben Fleuty <bfleuty@outlook.com>
2022-05-27 07:39:46 +0100

    handle client side validation of log in details

--- assessment/app/src/main/java/uk/ac/abertay/notsnapchat/LoginActivity.java
+++ assessment/app/src/main/java/uk/ac/abertay/notsnapchat/LoginActivity.java
@@ -1,8 +1,10 @@
 package uk.ac.abertay.notsnapchat;
 
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -28,13 +30,60 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void signIn(){
-        // todo input validation
+    private boolean isLoginInformationValid() {
+        boolean isValid = true;
 
+        // check email validity
+        isValid &= isEmailValid();
+
+        // check password validity
+        isValid &= isPasswordValid();
+
+        return isValid;
+    }
+
+    private boolean isEmailValid() {
+        String strEmail = getEmail();
+        return (!TextUtils.isEmpty(strEmail) && Patterns.EMAIL_ADDRESS.matcher(strEmail).matches());
+    }
+
+    private boolean isPasswordValid() {
+        String strPassword = getPassword();
+        return !TextUtils.isEmpty(strPassword);
+    }
+
+    private String getEmail() {
+        return ((EditText) findViewById(R.id.txtEmail)).getText().toString();
+    }
+
+    private String getPassword() {
+        return ((EditText) findViewById(R.id.txtPassword)).getText().toString();
+    }
+
+    private void signIn() {
+        // todo input validation
+        boolean success = true;
+        String errors = "";
+        if (!isEmailValid()) {
+            success = false;
+            errors += " Invalid email!";
+        }
+        if (!isPasswordValid()) {
+            success = false;
+            errors += " Invalid password!";
+
+        }
+
+        if (!success) {
+            Toast.makeText(this, errors, Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Toast.makeText(this, ("Logging in with " + getEmail() + "/" + getPassword()), Toast.LENGTH_LONG).show();
         // todo try sign in
     }
 
-    private void login(){
+    private void login() {
         // todo input validation
 
         // todo try log in
